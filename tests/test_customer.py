7857d1905719672facac2811883c5d9a7643fb60
= <kelmerpassos@gmail.com>
2022-07-21 11:20:11 -0300

    test: Testes de produtos do cliente

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -1,8 +1,10 @@
 
-from http.client import OK, UNAUTHORIZED, CREATED
+from http.client import OK, UNAUTHORIZED, CREATED, BAD_REQUEST
 from flask.testing import FlaskClient
+from pytest import mark
 
-from ipet.ext.customer.models import Customer
+from ipet.ext.customer.models import AssocProductCustomer, Customer
+from ipet.ext.customer.schemas import STATUS_CHOICE
 
 
 
@@ -82,3 +84,41 @@ def test_delete_customer(authentication, client: FlaskClient):
 
 def test_authentication_obligation_in_delete_customer(client: FlaskClient):
     assert client.delete("/customer/1", follow_redirects=True).status_code == UNAUTHORIZED
+
+def test_return_code_customer_products(authentication, client: FlaskClient):
+    assert client.get("/customer/1/product", headers=authentication, follow_redirects=True).status_code == OK
+
+def test_customer_products(authentication, client: FlaskClient):
+    json = client.get("/customer/1/product", headers=authentication, follow_redirects=True).get_json()
+    assert len(json["data"]["elements"]) > 0
+
+def test_authentication_obligation_in_customer_products(client: FlaskClient):
+    assert client.get("/customer/1/product", follow_redirects=True).status_code == UNAUTHORIZED
+
+def test_return_code_product_and_customer_information(authentication, client: FlaskClient):
+    assert client.get("/customer/1/product/1", headers=authentication, follow_redirects=True).status_code == OK
+
+def test_product_and_customer_information(authentication, client: FlaskClient):
+    json = client.get("/customer/1/product/1", headers=authentication, follow_redirects=True).get_json()
+    data = json["data"]
+    assert bool(data.get("customer")) and bool(data.get("product")) and data["currentStatus"] in STATUS_CHOICE
+
+def test_authentication_obligation_in_product_and_customer_information(client: FlaskClient):
+    assert client.get("/customer/1/product/1", follow_redirects=True).status_code == UNAUTHORIZED
+
+@mark.parametrize("status", STATUS_CHOICE)
+def test_return_code_update_customer_product_status(authentication, client: FlaskClient, status):
+    assert client.patch("/customer/1/product/1", headers=authentication, json={"currentStatus": status}, follow_redirects=True).status_code == OK
+    
+@mark.parametrize("status", ["IN PROGRESS", "CLOSED", "OPEN"])
+def test_return_code_update_customer_product_status(authentication, client: FlaskClient, status):
+    assert client.patch("/customer/1/product/1", headers=authentication, json={"currentStatus": status}, follow_redirects=True).status_code == BAD_REQUEST
+
+def test_update_customer_product_status(authentication, client: FlaskClient):
+    client.patch("/customer/1/product/1", headers=authentication, json={"currentStatus": "BLOCKED"}, follow_redirects=True).get_json()
+    assc = AssocProductCustomer.query.filter(AssocProductCustomer.customer_id==1, AssocProductCustomer.product_id==1).first()
+    assert assc.current_status == "BLOCKED"
+
+def test_authentication_obligation_in_update_customer_product_status(client: FlaskClient):
+    assert client.patch("/customer/1/product/1", json={"currentStatus": "ACTIVE"}, follow_redirects=True).status_code == UNAUTHORIZED
+
